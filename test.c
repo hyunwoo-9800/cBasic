096cccc604dd8a6df1106ac0ed56c25662beefea
hyunwoo-9800 <hyunwoo9800@naver.com>
2025-08-12 11:08:04 +0900

    [add] C언어 기초(컴파일 출력 테스트) 2025/08/12 @0.0h

---
+++ test.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+// 프로그램 설명: 기초 출력 테스트
+// 작성일: 2025/08/12
+// 작성자: Hyunwoo
+
+int main(int argc, char *argv[]) {
+
+    // 여기에 코드 작성
+    printf("Hello, C World!\n");
+    printf("C 언어 길라잡이\n");
+    system("PAUSE");
+
+    return 0;
+}
